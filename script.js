2c986408474611df7d9e52ded2264985b84ccae5
Christian Picoli <124415337+ChrisNoronha@users.noreply.github.com>
2024-05-24 21:59:58 -0300

    Scripts
    Foi retirado a parte em relação ao login e registro e foi adicionado as funcionalidades do slideshow

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+let slideIndex = 0;
+showSlides();
 
-    function validateForm() {
-        var password = document.getElementById("password").value;
-        var confirmPassword = document.getElementById("confirm-password").value;
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-        if (password !== confirmPassword) {
-            alert("As senhas não coincidem. Por favor, tente novamente.");
-            return false;
-        }
-        return true;
-    }
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  if (slideIndex >= slides.length) {slideIndex = 0}
+  if (slideIndex < 0) {slideIndex = slides.length - 1}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
+
+// Adicionando a função para trocar de slide automaticamente a cada 8 segundos
+setInterval(function() {
+  plusSlides(1); // Chama a função plusSlides com o argumento 1 para avançar o slide
+}, 8000); // 8000 milissegundos = 8 segundos
