aabaf1eaabea10b7417397fb8b7f3541d630e564
Christian Picoli <124415337+ChrisNoronha@users.noreply.github.com>
2024-05-26 14:35:53 -0300

    Mudança na função de slides
    A função de slides agora é adaptada para suportar múltiplos carrosséis

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 //funções responsáveis pelo carrossel
-let slideIndex = 0;
-showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// Function to show next slide
+function showNextSlide(carouselId) {
+    var carousel = document.getElementById(carouselId);
+    var activeSlide = carousel.querySelector('.slide.active');
+    var nextSlide = activeSlide.nextElementSibling;
+    if (!nextSlide) {
+        nextSlide = carousel.firstElementChild;
+    }
+    activeSlide.classList.remove('active');
+    nextSlide.classList.add('active');
 }
 
-function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("slide");
-  if (slideIndex >= slides.length) {slideIndex = 0}
-  if (slideIndex < 0) {slideIndex = slides.length - 1}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex].style.display = "block";
-}
+// Set intervals for carousels
+setInterval(function() {
+    showNextSlide('carousel1');
+}, 8000); // Change slide every 8 seconds
 
 setInterval(function() {
-  plusSlides(1); 
-}, 8000); //8 segundos para troca de slides
-
+    showNextSlide('carousel2');
+}, 8000); // Change slide every 8 seconds
 //funções responsáveis pelo carrossel(fim)
 
 //função responsável pelo botão de copiar
@@ -30,3 +29,27 @@ function copyToClipboard() {
     alert("Dados da doação copiados: " + textToCopy);
 }
 //função responsável pelo botão de copiar(fim)
+
+   // Carrega a API do YouTube
+    var tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // Variável para armazenar a referência ao player
+    var player;
+
+    // Função chamada quando a API do YouTube é carregada
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '360', // Altura do player
+            width: '640', // Largura do player
+            videoId: 'b_N3rSNg6HQ', // ID do vídeo do YouTube que você deseja reproduzir
+            playerVars: {
+                'autoplay': 1, // Reproduz automaticamente o vídeo quando o player é carregado
+                'controls': 1, // Mostra os controles do player
+                'rel': 0, // Não mostra vídeos relacionados ao final
+                'showinfo': 0 // Não mostra informações do vídeo
+            }
+        });
+    }
